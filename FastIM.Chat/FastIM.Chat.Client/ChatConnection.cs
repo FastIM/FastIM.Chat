66d97f7c5215bf9a20557f170d60a04abbdb7543
eddi2345 <tobicool51@gmail.com>
2015-04-22 21:05:05 +0200

    Updated Project Files.

---
+++ FastIM.Chat/FastIM.Chat.Client/ChatConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastIM.Chat.Client
+{
+    public class ChatConnection
+    {
+
+        /// <summary>
+        /// Erstellt eine neue Instanz der ChatConnection klasse.
+        /// </summary>
+        /// <param name="IP">Server ip</param>
+        /// <param name="port">Server port</param>
+        public ChatConnection(String IP, Int32 port)
+        {
+
+        }
+
+    }
+}
